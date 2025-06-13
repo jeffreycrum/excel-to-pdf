a747cb364c2d48f3132c25412e76635182a053ae
Jeff Crum <jeffrey.crum81@gmail.com>
2025-06-12 17:01:24 -0700

    add logic to gen pdf files and update gitignore

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-import pandas as pd
-
 import glob
+from pathlib import Path
+
+import pandas as pd
+from fpdf import FPDF
 
 filepaths = glob.glob("invoices/*.xlsx")
 
 for file in filepaths:
     df = pd.read_excel(file, sheet_name="Sheet 1")
-    print(df)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    filename = Path(file).stem
+    invoice_nr = filename.split("-")[0]
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
+    pdf.output(f"PDFs/{filename}.pdf")
